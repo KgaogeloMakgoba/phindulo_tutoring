09789ddd454635d1d27b2ad7076cceacf3f2787c
unknown <kgaogelomakgoba34@gmail.com>
2026-04-10 06:29:03 +0200

    Fixing the 'About' container layout

--- src/components/Home/Newsletter/index.tsx
+++ src/components/Home/Newsletter/index.tsx
@@ -8,10 +8,10 @@ const Newsletter = () => {
         <section id="about">
             <div className="container mx-auto lg:max-w-screen-xl md:max-w-screen-md px-4">
                 <div className="grid grid-cols-1 gap-y-10 gap-x-6 md:grid-cols-12 xl:gap-x-8">
-                    <div className={`col-span-12 ${isProd ? 'bg-newsletter-bg-2' : 'bg-newsletter-bg'} bg-contain bg-no-repeat`}>
-                        <div className="mb-10 mt-24 lg:mx-64 lg:my-24">
-                            <h3 className="text-4xl md:text-5xl text-center font-semibold text-white mb-3">About Us.</h3>
-                            <h3 className="text-base font-bold text-white/95 text-center mb-8">
+                    <div className={`col-span-12 ${isProd ? 'bg-newsletter-bg-2' : 'bg-newsletter-bg'} bg-cover bg-center bg-no-repeat min-h-[400px] lg:min-h-[500px]`}>
+                        <div className="mb-10 mt-16 lg:mx-64 lg:my-24 px-4 max-w-3xl mx-auto">
+                            <h3 className="text-2xl sm:text-3xl md:text-5xl text-center font-semibold text-white mb-3">About Us.</h3>
+                            <h3 className="text-sm sm:text-base md:text-lg font-medium text-white/95 text-center mb-8 leading-relaxed break-all">
                                 Phindulo was discovered in 2021,and since then we've been transforming lives through education,employment,and character development.With partnerships like Anzisha Price,we are bridging the gap between education and employment.Our community is built on the principles of empowerment,innovation and collaboration.We are proud to have empowered hundreds of students to reach their full potential!From academic support to career guidance,we have helped our students gain confidence in thier abilities,improve thier academic perfomance and develop essential life skills.Also they have secured intenships,job placements and have become leaders in thier communities.Be part of our story and help us create a brighter future for African youth! <br/>#PhinduloJourney#YouthEmpowerment#EducationForEmployment.
                             </h3>
                         </div>
